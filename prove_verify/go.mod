8413a32fc93ab13d6a19c2607fb57529328788db
ngocban <daongocban1102@gmail.com>
2023-03-15 11:37:59 +0700

    verify ok

--- prove_verify/go.mod
+++ prove_verify/go.mod
@@ -4,4 +4,10 @@ go 1.19
 
 require github.com/iden3/go-rapidsnark/prover v0.0.9
 
-require github.com/iden3/go-rapidsnark/types v0.0.2 // indirect
+require (
+	github.com/ethereum/go-ethereum v1.10.26 // indirect
+	github.com/iden3/go-iden3-crypto v0.0.13 // indirect
+	github.com/iden3/go-rapidsnark/types v0.0.2 // indirect
+	github.com/iden3/go-rapidsnark/verifier v0.0.3 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
--- prove_verify/main.go
+++ prove_verify/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	prover "github.com/iden3/go-rapidsnark/prover"
+	"github.com/iden3/go-rapidsnark/verifier"
 )
 
 func main() {
@@ -18,21 +19,20 @@ func main() {
 		panic(err)
 	}
 
+	vetificationKey, err := os.ReadFile("./verification_key.json")
+	if err != nil {
+		panic(err)
+	}
 	// use typeProof because prover.Groth16Prover returns a typeProof.ZKProof
 	proof, err := prover.Groth16Prover(provingKey, witness)
 	if err != nil {
 		panic(err)
 	}
 
-	// print proof . proof is a typeProof.ZKProof
-	print("proof:", proof.Proof)
-	print("\n")
-	print("proof.A:", proof.Proof.A)
-	print("\n")
-	print("proof.B:", proof.Proof.B)
-	print("\n")
-	print("proof.C:", proof.Proof.C)
-	print("\n")
-	print("publicSignals:", proof.PubSignals)
-	print("\n")
+	//verifier
+	err = verifier.VerifyGroth16(*proof, vetificationKey)
+	if err != nil {
+		panic(err)
+	}
+	print("Proof verified!")
 }
